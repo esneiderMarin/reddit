74e609ca8e422b2e21cd98531f66dce51175f46f
Esneider <esneider.m97@gmail.com>
2020-06-02 20:14:36 -0500

    Updates

---
+++ precache-manifest.8101f7f82f8a8be3d1b239998ffe1c98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba31f48ad98ff01fdde0ee221093fc0",
+    "url": "/reddit/index.html"
+  },
+  {
+    "revision": "23cc7b15e52767191794",
+    "url": "/reddit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cde425282c9378eda12a",
+    "url": "/reddit/static/js/2.e379a963.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reddit/static/js/2.e379a963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23cc7b15e52767191794",
+    "url": "/reddit/static/js/main.cfa8268b.chunk.js"
+  },
+  {
+    "revision": "41ae45721f39ed7f4368",
+    "url": "/reddit/static/js/runtime-main.2939e59a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reddit/static/media/logo.5d5d9eef.svg"
+  }
+]);
